c5d255c9c32f9ec7a1eca061ffacdfc4df7a255f
Basil Suter <git@nadar.io>
2016-11-11 13:21:17 +0100

    unify luya text info, added module and active window tests.

--- console/commands/views/aw/create.php
+++ console/commands/views/aw/create.php
@@ -10,37 +10,42 @@
 echo "<?php\n";
 ?>
 
-namespace <?php echo $namespace; ?>;
+namespace <?= $namespace; ?>;
+
+use Yii;
+use luya\admin\ngrest\base\ActiveWindow;
 
 /**
- * ActiveWindow created with LUYA Version <?= $luya; ?>.
+ * <?= $alias; ?>.
+ *
+ * <?= $luyaText; ?> 
  */
-class <?php echo $className; ?> extends \luya\admin\ngrest\base\ActiveWindow
+class <?= $className; ?> extends ActiveWindow
 {
     /**
      * @var string The name of the module where the ActiveWindow is located in order to finde the view path.
      */
-	public $module = '@<?php echo $moduleId; ?>';
+    public $module = '@<?= $moduleId; ?>';
 	
     /**
      * @var string The name of of the ActiveWindow. This is displayed in the CRUD list.
      */
-	public $alias = '<?php echo $alias; ?>';
+    public $alias = '<?= $alias; ?>';
 	
     /**
      * @var string The icon name from goolges material icon set (https://material.io/icons/)
      */
-	public $icon = 'extension';
+    public $icon = 'extension';
 	
-	/**
+    /**
      * The default action which is going to be requested when clicking the ActiveWindow.
      * 
      * @return string The response string, render and displayed trough the angular ajax request.
      */
-	public function index()
-	{
-		return $this->render('index', [
-			'id' => $this->itemId,
-		]);
-	}
+    public function index()
+    {
+        return $this->render('index', [
+            'id' => $this->itemId,
+        ]);
+    }
 }
